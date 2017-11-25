bb8d2ffde6abae2134026afa92c075120bd13d7c
Felipe De la Fuente <fadelafuente1@gmail.com>
2017-11-25 03:05:31 -0300

    add default format for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products, defaults: { format: 'json' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :images, defaults: { format: 'json' }
