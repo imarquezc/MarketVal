cfef98c0c144ee42a330bd03a895fa58f71ad229
dagalemiri <dagalemiri@uc.cl>
2017-11-25 03:01:52 -0300

    Fixed json problem

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,7 +3,8 @@ class ImagesController < ApplicationController
     skip_before_action :verify_authenticity_token  
 
     def create
-        image = Image.create(image: params[:image])
+        puts params[:image]
+        image = Image.new(image: params[:image])
         if image.save
             respond_to do |format|
                 msg = { :tags => ["tag1", "tag2"]}
@@ -12,7 +13,7 @@ class ImagesController < ApplicationController
         else
             respond_to do |format|
                 msg = { :error => "An error has occurred, please try again" }
-                format.json  { render :json => msg } # don't do msg.to_json
+                format.json  { render :json => msg} # don't do msg.to_json
               end
         end
     end
@@ -20,16 +21,14 @@ class ImagesController < ApplicationController
     def index
         images = Image.all
         respond_to do |format|
-            msg = { :images => images}
-            format.json  { render :json => msg } # don't do msg.to_json
+            format.json  { render :json => images.to_json} # don't do msg.to_json
           end
     end
 
     def show
         image = Image.find(params[:id])
         respond_to do |format|
-            msg = { :image => image}
-            format.json  { render :json => msg } # don't do msg.to_json
+            format.json  { render :json => image.to_json } # don't do msg.to_json
         end
     end
 
