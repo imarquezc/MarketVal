52b60137aa9ddc614229c382e31214c03d6b7228
imarquezc <ignaciomarquezc@gmail.com>
2017-11-25 03:22:43 -0300

    added get_tags for images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,27 @@
 class Image < ApplicationRecord
     belongs_to :product
 end
+
+require "base64"
+require 'net/http'
+
+uri = URI('https://brazilsouth.api.cognitive.microsoft.com/vision/v1.0/analyze')
+uri.query = URI.encode_www_form({
+    'visualFeatures' => 'Categories,Tags',
+    'language' => 'en'
+})
+
+def get_tags()
+	plain = Base64.decode64(self.image.partition(',').last)
+	request = Net::HTTP::Post.new(uri.request_uri)
+	# Request headers
+	request['Content-Type'] = 'application/octet-stream'
+	# Request headers
+	request['Ocp-Apim-Subscription-Key'] = '6c99c2861bc7448395d807e7ee13e2fd'
+	# Request body
+	request.body = plain
+	response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+	    http.request(request)
+	end
+	return response.body
+end
